a88396aea39301cfd3d5eb8fdbc0600c1ef7ded1
devlogerio <devloger.io@gmail.com>
2019-03-11 08:11:01 -0700

    Add POST /todos route and refactor mongoose

---
+++ server/db/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost:27017/';
+const dbName = 'TodoApp';
+mongoose.Promise = global.Promise;
+mongoose.connect(url+dbName, {useNewUrlParser: true});
+
+ module.exports =  {mongoose}
