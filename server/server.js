92bc508c2e42c88561a1d53ffc5c82e53d6c3b4e
devlogerio <devloger.io@gmail.com>
2019-03-13 08:26:09 -0700

    Add GET todos/:id

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ var bodyParsaer = require('body-parser');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/todo');
+var {User} = require('./models/users');
+
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -20,6 +22,23 @@ app.post('/todos', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    
+    User.findById(id).then((user) => {
+        if(!user){
+            return res.status(404).send();
+        }
+        return res.send(user);
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
